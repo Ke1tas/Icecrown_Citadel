9cda7fdfece8a39a12ebe83fc0a0b113f4ce500b
Keltas <inozemtseeev@gmail.com>
2025-04-15 18:09:00 +0400

    lab2

--- constants.py
+++
@@ -1,8 +0,0 @@
-ALPHABET = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .,-'
-DECRYPTED_TEXT_TXT = 'decrypted_text.txt'
-DECRYPTION_KEY_TXT = 'decryption_key.txt'
-ENCODED_MESSAGE_TXT = 'encoded_message.txt'
-ENCRYPTED_TEXT_TXT = 'cod9.txt'
-ENCRYPTION_KEY_TXT = "encryption_key.txt"
-ORIGINAL_MESSAGE_TXT = "my_text.txt"
-TABLE_FREQUENCY_TXT = 'table_frequency.txt'
